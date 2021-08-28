0be6681b676ba555e30f907efbe918f749ec564b
Soumalya Bhattacharya <59192460+soumalyatheking22012001@users.noreply.github.com>
2021-08-28 23:16:09 +0530

    changing typo in routes

--- Routes/routes.js
+++ Routes/routes.js
@@ -7,7 +7,7 @@ router.use("/signup", require("../API/signup"));
 router.use("/verify", require("../API/verify"));
 router.use("/logout", require("../API/logout"));
 router.use("/getBooking", require("../API/getbooking"));
-router.use("/getBookingByUser", require("../API/getbookingbyuser"));
+router.use("/getBookingByUser", require("../API/getBookingByUser"));
 router.use("/createBooking", require("../API/createbooking"));
 router.use("/createHalls", require("../API/createhalls"));
 router.use("/searchBooking", require("../API/getBookingsbyDate"));
